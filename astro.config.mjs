2eeb8c6bc4e447f7348ea87f2453422b3cb92fcd
adrian <adriansilverslatter@gmail.com>
2025-04-07 19:23:54 -0600

    cambio de dominio

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://raul-adb.github.io/',
-  base: 'afa_preview',
+  base: 'afa_prev',
   i18n: {
     locales: ['en', 'es'],
     defaultLocale: 'es',
